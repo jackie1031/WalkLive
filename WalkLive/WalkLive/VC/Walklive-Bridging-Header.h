61b095f4069fbfe62e6e39091345490232cc26eb
Michelle <mshu1@jhu.edu>
2017-10-12 18:59:49 -0400

    new working branch

---
+++ WalkLive/WalkLive/VC/Walklive-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Walklive-Bridging-Header.h
+//  Walklive
+//
+//  Created by Michelle Shu on 10/12/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+#ifndef Walklive_Bridging_Header_h
+#define Walklive_Bridging_Header_h
+
+
+#endif /* Walklive_Bridging_Header_h */
