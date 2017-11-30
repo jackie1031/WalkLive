f4c21908f9f6a5df17d89511fb61790499860e3e
Michelle <mshu1@jhu.edu>
2017-11-30 01:18:35 -0500

    add emergency number, setting controller, refactor

--- WalkLive/WalkLive/VC/UserVC.swift
+++ WalkLive/WalkLive/VC/UserVC.swift
@@ -9,27 +9,53 @@
 import UIKit
 
 class UserVC: UIViewController {
-
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var userContactLabel: UILabel!
+    
+    @IBOutlet weak var emergencyContactLabel: UILabel!
+    @IBOutlet weak var friendRequestButton: UIButton!
+    @IBOutlet weak var tripRequestButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func onMyContactPencilButton(_ sender: Any) {
+        segueToSettingVC()
+    }
+    
+    @IBAction func onEmerContactPencilButton(_ sender: Any) {
+        segueToSettingVC()
+    }
+    
+    @IBAction func onAddFriendButton(_ sender: Any) {
+        segueToSettingVC()
+    }
+    
+    func segueToSettingVC(){
+        performSegue(withIdentifier: "settingVCSegue", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
---
+++ WalkLive/Walklive/VC/SettingVC.swift
@@ -0,0 +1,42 @@
+//
+//  SettingVC.swift
+//  Walklive
+//
+//  Created by Michelle Shu on 11/30/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onSaveButton(_ sender: Any) {
+    }
+
+    @IBAction func onCancelButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
