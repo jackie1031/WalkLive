cc216ae6710e797bcdbd1e35dd3ea8241768de50
Michelle <mshu1@jhu.edu>
2017-12-04 12:53:30 -0500

    finished timer on starting trip, time manager model, update trip panel board

---
+++ WalkLive/Walklive/Delegates/TripPanelDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TripPanelDelegate.swift
+//  Walklive
+//
+//  Created by Michelle Shu on 12/4/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import Foundation
+protocol TripPanelDelegate: class {
+    func updateTripPanel(timeInfo: String)
+}
--- WalkLive/Walklive/VC/MainMapVC.swift
+++ WalkLive/Walklive/VC/MainMapVC.swift
@@ -22,6 +22,8 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     var locationManager = CLLocationManager()
     var roadRequester = RoadRequester()
+    var timeManager: TimeManager!
+    var tripView: OnGoingTripView!
     var mapItems: [MKMapItem]!
 
     override func viewDidLoad() {
@@ -35,11 +37,13 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         self.hidePanels()
     }
     
+    /// Private functiosn for general logistics
     private func setDelegate() {
         self.mapView.delegate = self
         self.locationManager.delegate = self
     }
     
+
     private func hidePanels(){
         self.startTripPanelView.isHidden = true
         self.contactMessagePanel.isHidden = true
@@ -57,10 +61,6 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         self.view.addGestureRecognizer(hideTap)
     }
     
-    @objc func hideKeyboardTap(_ recoginizer: UITapGestureRecognizer) {
-        self.view.endEditing(true)
-    }
-    
     /// Check authrization status and start update locations
     private func authorizeLocationUpdate() {
         locationManager.delegate = self
@@ -74,6 +74,17 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         }
     }
     
+    //Public functions
+
+    /// hides keyboard when user finish editing and tap on other places on the screen
+    ///
+    /// - Parameters:
+    ///   - recoginizer: object to recognize motion when user tap on the screen
+    @objc func hideKeyboardTap(_ recoginizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
     
     /// Start update location if authorized
     ///
@@ -86,7 +97,10 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         }
     }
     
-    
+    /// Show/hide start trip panel when start trip button is clicked
+    ///
+    /// - Parameters:
+    ///  Sender: the button which receives this action
     @IBAction func onStartTripBottomButton(_ sender: Any) {
         startTripPanelView.isHidden = false
         UIView.animate(withDuration: 0.6) {
@@ -94,6 +108,10 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         }
     }
     
+    /// Show/hide message panel when start trip button is clicked
+    ///
+    /// - Parameters:
+    ///  Sender: the button which receives this action
     @IBAction func onContactBottomButton(_ sender: Any) {
         let message = buildMessage(location: roadRequester.getSourceLocation())
         
@@ -101,6 +119,10 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         self.contactMessagePanel.isHidden = false
     }
     
+    /// boost emergency call(911) when police button is clicked
+    ///
+    /// - Parameters:
+    ///  Sender: the button which receives this action
     @IBAction func onPoliceBottomButton(_ sender: Any) {
         let busPhone = "911"
         //Does not work in Simulator
@@ -113,14 +135,26 @@ class MainMapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
         }
     }
     
+    /// send message to user when send button is clicked
+    ///
+    /// - Parameters:
+    ///  Sender: the button which receives this action
     @IBAction func onContactPanelSendButton(_ sender: Any) {
         self.contactMessagePanel.isHidden = true
     }
     
+    /// build messages based on user location
+    ///
+    /// - Parameters:
+    ///  location: location needed to form this message
     func buildMessage(location: CLLocation) -> String {
         return "I am currently at Time Square(latitude:" +  String(location.coordinate.latitude) + ", longitude: " + String(location.coordinate.longitude) + "), and heading to Empire State Building(). Need ~15 minutes, I have walked 0 minutes, 5 seconds. From Admin."
     }
     
+    /// request route + location when start button is clicked on start trip panel
+    ///
+    /// - Parameters:
+    ///  sender: the button which is function is triggered
     @IBAction func onStartTripTopButton(_ sender: Any) {
         //check destination emptiness
         if (self.startTripDestinationTextLabel.text == "") {
@@ -183,23 +217,40 @@ extension MainMapVC: RouteDelegate{
         }, destinationMapItem: targetItem)
     }
     
-    func startTrip(trip: Trip){
+    private func startTrip(trip: Trip){
+        self.createTripView(trip: trip)
+        self.createTimeManager(trip: trip)
+    }
+    
+    private func createTripView(trip: Trip){
         let tv = OnGoingTripView()
-        let tripView = tv.loadNib()
-//        self.startTripPanelView.addSubview(tripView)
-        tripView.destinationLabel.text = trip.destinationName
-        tripView.addressLabel.text = trip.address
-        tripView.estimatedTimeLabel.text = "Estimated Time: " + (trip.timeInterval?.description)!
+        self.tripView = tv.loadNib()
+        self.tripView.destinationLabel.text = trip.destinationName
+        self.tripView.addressLabel.text = trip.address
+        self.tripView.estimatedTimeLabel.text = "Estimated Time: " + String(Int((trip.timeInterval!))/60) + " min(s)"
         
-        tripView.center = self.startTripPanelView.center
+        self.tripView.center = self.startTripPanelView.center
         
-        tripView.routeDelegate = self
+        self.tripView.routeDelegate = self
         self.startTripPanelView.addSubview(tripView)
+    }
+    
+    private func createTimeManager(trip: Trip){
+        self.timeManager = TimeManager(timeInterval: trip.timeInterval!)
+        self.timeManager.tripPanelDelegate = self
+        self.timeManager.startTimer(timeInterval: 60)
     }
     
     
     func cancelTrip(){
         self.roadRequester.removeRoute()
+        self.timeManager.endTimer()
+    }
+}
+
+extension MainMapVC: TripPanelDelegate{
+    func updateTripPanel(timeInfo: String) {
+        self.tripView.timeUsedLabel.text = timeInfo
     }
 }
 
