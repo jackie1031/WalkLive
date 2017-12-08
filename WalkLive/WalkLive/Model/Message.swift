f8d7b60bfda808b32748ff9aae7be2597f1866fd
YangCao1708 <samcy1708@gmail.com>
2017-12-07 22:46:12 -0500

    text message

---
+++ WalkLive/WalkLive/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  WalkLive
+//
+//  Created by Yang Cao on 12/7/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import Foundation
---
+++ WalkLive/WalkLive/VC/MessageVC.swift
@@ -0,0 +1,45 @@
+//
+//  MessageVC.swift
+//  WalkLive
+//
+//  Created by Yang Cao on 12/7/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import UIKit
+
+class MessageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onAddTextButton(_ sender: Any) {
+        // need to get the coordinates of the last textfield
+    
+        var frame = CGRect(x: xCoor, y: yCoor, width: w, height: h)
+        let text = UITextField(frame: frame)
+        let view = UIView(frame: frame)
+        view.addSubview(text)
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
