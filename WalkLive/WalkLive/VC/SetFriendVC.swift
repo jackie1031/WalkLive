7b8d6339b60b5b4cd8d36ea28534efc88029eff0
Michelle <mshu1@jhu.edu>
2017-12-10 15:07:23 -0500

    friend table

---
+++ WalkLive/WalkLive/VC/SetFriendVC.swift
@@ -0,0 +1,42 @@
+//
+//  SetFriendVC.swift
+//  WalkLive
+//
+//  Created by Michelle Shu on 12/10/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import UIKit
+
+class SetFriendVC: UIViewController {
+    
+    @IBOutlet weak var currentFriendNameLabel: UILabel!
+    @IBOutlet weak var currentEmergencyNumberLabel: UILabel!
+    @IBOutlet weak var friendListTable: UITableView!
+    
+    
+    var friends: [Friend]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
