738aa815f2120b296d1ecf9a965ca5ce2ff51110
Michelle <mshu1@jhu.edu>
2017-12-16 19:29:25 -0500

    add trip history

---
+++ WalkLive/WalkLive/Cell/TripHistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripHistoryTableViewCell.swift
+//  WalkLive
+//
+//  Created by Michelle Shu on 12/16/17.
+//  Copyright © 2017 OOSE-TEAM14. All rights reserved.
+//
+
+import UIKit
+
+class TripHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
